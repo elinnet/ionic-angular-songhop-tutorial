f58740afcc9a3c262eef2d9a6de9ea8d1a028873
Elin <elinnet@gmail.com>
2015-10-05 17:16:59 +0100

    loading indicators for loading of song and artwork work

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,34 +4,58 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
+.controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
+  // helper functions for loading
+  var showLoading = function(){
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  };
+
+  var hideLoading = function(){
+    $ionicLoading.hide();
+  };
+
+  // set loading to true first time while we retrieve songs from server
+  showLoading();
+
+
   // get our first songs
   Recommendations.init()
     .then(function(){
 
       $scope.currentSong = Recommendations.queue[0];
-      Recommendations.playCurrentSong();
-    });
 
+      return Recommendations.playCurrentSong();
+    })
+    .then(function(){
+      // turn loading off
+        hideLoading();
+        $scope.currentSong.loaded = true;
+    });
 
     // fired when we favorite / skips a song
       $scope.sendFeedback = function(bool){
 
         // first, add to favorites if they favorited
         if(bool) User.addSongToFavorites($scope.currentSong);
-
+        //set variable for the correct animation sequence
         $scope.currentSong.rated = bool;
         $scope.currentSong.hide = true;
 
         // prepare the next song
         Recommendations.nextSong();
 
+        // update current song in scope, timeout to allow animation to complete
         $timeout(function(){
           //$timeout to allow animation to complete
           $scope.currentSong = Recommendations.queue[0];
         }, 250);
 
-        Recommendations.playCurrentSong();
+        Recommendations.playCurrentSong().then(function(){
+          $scope.currentSong.loaded = true;
+        });
       };
 
     $scope.nextAlbumImg = function(){
